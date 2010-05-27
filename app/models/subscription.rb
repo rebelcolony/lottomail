37127cafe773cfe2179da1c5a55b0da9d0eedbf7
kevin bett <kevin@rebelcolony.com>
2010-05-27 11:29:17 +0600

    setup rspec and added login button

--- app/models/subscription.rb
+++ app/models/subscription.rb
@@ -1,3 +1,4 @@
 class Subscription < ActiveRecord::Base
   has_and_belongs_to_many :users
+  validates_presence_of :name, :subject
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -4,11 +4,25 @@ describe User do
   before(:each) do
     @valid_attributes = {
       :name => "value for name",
-      :email => "value for email"
+      :email => "kevin@hotmail.com"
     }
   end
 
   it "should create a new instance given valid attributes" do
     User.create!(@valid_attributes)
   end
+  
+  it "should fail when name is missing" do
+    @valid_attributes.delete(:name)
+    lambda { @user = User.create!(@valid_attributes) }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+   
+  it "should fail when email is missing" do
+    @valid_attributes.delete(:email)
+    lambda { @user = User.create!(@valid_attributes) }.should raise_error(ActiveRecord::RecordInvalid)
+  end
+  
+  it "should fail when a user has not selected anything" do
+  end
+  
 end
